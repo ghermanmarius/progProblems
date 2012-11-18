fc0ac54d0e92e4a5bd6d559c183bc2558a10aa1a
marius <mariusgherman8@gmail.com>
2012-11-18 19:09:25 +0200

    rosalind GC solution

--- rosalind/gc.cpp
+++ rosalind/gc.cpp
@@ -2,26 +2,33 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    ifstream inFile;
-    inFile.open("rosalind_gc.txt");
-    string id, content;
-    while (!inFile.eof())
-    {
-        inFile >> id;
-        inFile >> content;
     
+    std::ifstream inFile("rosalind_gc.txt");
+    string id, content;
+    float max = 0.f;
+    int idMax;
+    std::vector<string> idList;
+    while (inFile >> id >> content)
+    {
          int numC = count(content.begin(), content.end(), 'C');
          int numG = count(content.begin(), content.end(), 'G');
+         idList.push_back(id);
     
-         cout << id.substr(1) << endl;
-         cout << (float(numC) + float(numG)) / content.size() * 100 << endl;
-
+         if ((float(numC) + float(numG)) * 100 / content.size() > max)
+         {
+            max = (float(numC) + float(numG)) * 100 / content.size();
+            idMax = idList.size();
+         }
     }
+
+    printf("%2.6f\n", max);
+    cout <<  static_cast<string>(idList.at(idMax - 1));
     inFile.close();    
     return 0;
 }
