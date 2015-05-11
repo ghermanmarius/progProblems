af7604c8c1c111b032a2067c5719facc5d4308fc
Marius Gherman <mariusgherman8@gmail.com>
2015-05-11 15:39:07 +0300

    updated sudoku.cpp

--- projectEuler/96/sudoku.cpp
+++ projectEuler/96/sudoku.cpp
@@ -13,6 +13,14 @@ void printGrid(int** grid) {
 		}
 		std::cout << std::endl;
 	}
+	std::cout << std::endl;
+}
+
+void printArray(int* arr, const char* arrName) {
+	for (int i = 0; i < kGridSize; ++i) {
+		std::cout << arrName << "[" << i << "] : " << arr[i] << std::endl;
+	}
+	std::cout << std::endl;
 }
 
 int main() {
@@ -23,7 +31,10 @@ int main() {
 		grid[i] = new int[kGridSize];
 	} 
 	int* rowMasks = new int[kGridSize];
-	for (int i = 0; i < kGridSize; ++i) rowMasks[i] = 0;
+	int* colMasks = new int[kGridSize];
+	memset(rowMasks, 0, kGridSize * sizeof(int));
+	memset(colMasks, 0, kGridSize * sizeof(int));
+	
 	//read grid number
 	char buffer[256];
 	fileStream.getline(buffer, 256);
@@ -35,15 +46,27 @@ int main() {
 			grid[i][j] = val ? (freeCellMask & (1 << val)) : freeCellMask;
 			if (val) {
 				rowMasks[i] |= (1 << val);
+				colMasks[j] |= (1 << val);
 			}
+			std::cout << val << " ";
 		}
-		fileStream.get(currentChar);
+		fileStream.getline(buffer, 256);
+		std::cout << std::endl;
+		//std::cout << currentChar;
 	}
+	
 	printGrid(grid);
-	for (int i = 0; i < kGridSize; ++i) {
-		std::cout << "row " << i << " -> " << rowMasks[i] << std::endl;
-	}
+	printArray(rowMasks, "rowMasks");
+	printArray(colMasks, "colMasks");
+	
+	applyMasks(grid, rowMasks, colMasks);
 	
 	
+	delete[] rowMasks;
+	delete[] colMasks;
+	for (int i = 0; i < kGridSize; ++i) { 
+		delete[] grid[i];
+	} 
+	delete[] grid;
 	return 0;
 }
